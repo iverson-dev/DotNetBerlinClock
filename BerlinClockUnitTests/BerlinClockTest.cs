1d4ae8dd27715efa192e762f54920a75221704ea
Iverson Panasiewicz <iverson.dev@gmail.com>
2019-12-17 22:55:49 -0300

    Unit Tests project and basic unit tests.

---
+++ BerlinClockUnitTests/BerlinClockTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using BerlinClock.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BerlinClock.UnitTests
+{
+    [TestClass]
+    public class BerlinClockImplTest
+    {
+        [TestMethod]
+        public void TestFistRowOfLampsIsLit()
+        {
+            //Test if first row lamp is switched off
+            string aTime = "13:00:01";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("O", clock.FirstRow());
+        }
+
+        [TestMethod]
+        public void TestFistRowOfLampsIsOff()
+        {
+            //Test if first row lamp is switched on
+            string aTime = "13:00:02";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("Y", clock.FirstRow());
+        }
+
+        [TestMethod]
+        public void TestSecondRowOfLampsIsLit()
+        {
+            //Test if second row lamps are all switched on
+            string aTime = "21:50:02";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("RRRR", clock.SecondRow());
+        }
+
+        [TestMethod]
+        public void TestSecondRowOfLampsIsOff()
+        {
+            //Test if second row lamps are all switched off
+            string aTime = "04:03:02";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("OOOO", clock.SecondRow());
+        }
+
+        [TestMethod]
+        public void TestThirdRowOfLampsIsLit()
+        {
+            //Test if third row lamps are all switched on
+            string aTime = "04:59:59";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("RRRR", clock.ThirdRow());
+        }
+
+        [TestMethod]
+        public void TestThirdRowOfLampsIsOff()
+        {
+            //Test if third row lamps are all switched off
+            string aTime = "00:00:00";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("OOOO", clock.ThirdRow());
+        }
+
+
+        [TestMethod]
+        public void TestFourthRowOfLampsIsLit()
+        {
+            //Test if fourth row lamps are all switched on
+            string aTime = "04:59:59";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("YYRYYRYYRYY", clock.FourthRow());
+        }
+
+        [TestMethod]
+        public void TestFourthRowOfLampsIsOff()
+        {
+            //Test if fourth row lamps are all switched off
+            string aTime = "00:04:00";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("OOOOOOOOOOO", clock.FourthRow());
+        }
+
+        [TestMethod]
+        public void TestFifthRowOfLampsIsLit()
+        {
+            //Test if fourth row lamps are all switched on
+            string aTime = "08:59:59";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("YYYY", clock.FifthRow());
+        }
+
+        [TestMethod]
+        public void TestFifthRowOfLampsIsOff()
+        {
+            //Test if fourth row lamps are all switched off
+            string aTime = "00:05:00";
+
+            BerlinClockImpl clock = new BerlinClockImpl(aTime);
+
+            Assert.AreEqual("OOOO", clock.FifthRow());
+        }
+    }
+}
---
+++ Classes/BerlinClockImpl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace BerlinClock.Classes
+{
+    public class BerlinClockImpl
+    {
+
+        public BerlinClockImpl(string aTime)
+        {
+
+        }
+
+        public string FirstRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SecondRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ThirdRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FourthRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FifthRow()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
